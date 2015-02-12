1cd5514f9c0b463aee364d14caefdeb7d68f506a
CrsOystr <nicmetz@jackmoves.com>
2015-02-12 14:20:27 -0700

    First commit

---
+++ Assets/ISOGRID.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ISOGRID : MonoBehaviour {
+
+	public Transform CellPrefab;
+	public Vector3 Size;
+	public Transform[,] Grid;
+	// Use this for initialization
+	void Start () {
+		CreateGrid ();
+	}
+
+	void CreateGrid(){
+		Grid = new Transform[(int)Size.x,(int)Size.y];
+		for (int y= 0; y < Size.y; y++) {
+			for (int x = 0; x < Size.x; x++) {
+				Transform newCell;
+				newCell = (Transform)Instantiate (CellPrefab, new Vector3 ((x - y) * 4 , (x + y)*2, 0), Quaternion.identity); 
+				newCell.name = string.Format("({0},{1})",x,y);
+				newCell.parent = transform;
+				newCell.GetComponent<TileScript>().Position = new Vector2(x,y);
+				Grid[x,y] = newCell;
+			}
+		}
+	}
+
+}
---
+++ Assets/TileScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileScript : MonoBehaviour {
+
+	public List<Transform> Adjacents;
+	public Vector3 Position;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
