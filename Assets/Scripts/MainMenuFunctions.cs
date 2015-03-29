90463a2de2be9ff3f532ff4f438fe34243684027
harleyb16 <harleyb16@gmail.com>
2015-03-29 15:26:25 -0600

    Basic GUI Main menu
    Implemented a basic main menu scene. Still need final assets/lots of
    work

---
+++ Assets/Scripts/MainMenuFunctions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeScene(int newScene)
+	{
+		Application.LoadLevel(newScene);
+	}
+}
